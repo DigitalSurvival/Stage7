20d09327edd39566cf243d836d56a930569d4a53
Matthew Marchese <Maffblaster@gmail.com>
2014-08-15 16:55:56 -0700

    Started work on the getStage7 script.

--- getStage7.sh
+++ getStage7.sh
@@ -2,3 +2,13 @@
 
 # Automatic download script is a work in progress...
 
+# Generates the package version from the version variable of the main script
+# This will always be up-to-date.
+VERSION=`grep VERSION scripts/stage7.sh | awk -F= '{ print $2 }'`
+
+wget https://github.com/DigitalSurvival/Stage7/raw/master/packages/stage7-${VERSION}.tar.xz .
+
+mkdir ~/stage7
+tar xpfJ stage7-${VERSION}.tar.xz -C ~/stage7
+
+echo -e "Stage7 has been downloaded and extracted.\nIn order for it to work properly you will need to run it with sudo permissions."
