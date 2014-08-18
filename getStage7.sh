dc675fcb4e54cc7ccdf7d398e98f005f7912714f
Matthew Marchese <Maffblaster@gmail.com>
2014-08-18 00:08:18 -0700

    Updated the getStage7.sh download script.
    Updated the todo.

--- getStage7.sh
+++ getStage7.sh
@@ -9,6 +9,12 @@ VERSION=`grep VERSION scripts/stage7.sh | awk -F= '{ print $2 }'`
 wget https://github.com/DigitalSurvival/Stage7/raw/master/packages/stage7-${VERSION}.tar.xz .
 
 mkdir ~/stage7
-tar xpfJ stage7-${VERSION}.tar.xz -C ~/testing
+tar xpfJ stage7-${VERSION}.tar.xz -C /stage7
 
-echo -e "Stage7 has been downloaded and extracted.\nIn order for it to work properly you will need to run it with sudo permissions."
+#todo
+echo -e "Stage7 has been downloaded and extracted to a directory created at /stage7\n"
+echo -e "In order for it to work properly you will need to run it with sudo permissions."
+echo -e "\nIf you know what you're doing move to /stage7 and start the script with \"sudo ./stage7.sh\""
+echo -e "If not, then you should read the wiki at https://github.com/DigitalSurvival/Stage7/wiki"
+echo -e "Or visit the website at http://www.digitalsurvival.us/stage7"
+
