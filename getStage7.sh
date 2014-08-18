40e1e383a3206404e2d733002e3cb5276cd2224e
Matthew Marchese <Maffblaster@gmail.com>
2014-08-18 16:18:57 -0700

    What in the whole world did I think I was doing?
    That doesn't even come close to working.

--- getStage7.sh
+++ getStage7.sh
@@ -2,9 +2,8 @@
 
 # Automatic download script is a work in progress...
 
-# Generates the package version from the version variable of the main script
-# The versioning should always be up-to-date this way.
-VERSION=`grep VERSION scripts/stage7.sh | awk -F= '{ print $2 }'`
+# Controls "release" version.
+VERSION=0.1.0
 
 wget https://github.com/DigitalSurvival/Stage7/raw/master/packages/stage7-${VERSION}.tar.xz .
 
